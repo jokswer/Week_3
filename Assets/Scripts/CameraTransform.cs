bc53b441d0044bdf8f2a5b700a294c8bddb6257c
jokswer <jokswer@gmail.com>
2023-02-08 20:07:00 +0700

    feat: add camera transform

---
+++ Assets/Scripts/CameraTransform.cs
@@ -0,0 +1,18 @@
+using Player;
+using UnityEngine;
+
+public class CameraTransform : MonoBehaviour
+{
+        [SerializeField] private Vector3 offset;
+        private Transform _target;
+
+        private void Start()
+        {
+                _target = FindObjectOfType<PlayerView>().transform;
+        }
+
+        private void LateUpdate()
+        {
+                transform.position = _target.position + offset;
+        }
+}
