21cac6bc9f845e86cf731e74ff733f95d19df776
jokswer <jokswer@gmail.com>
2023-02-14 22:59:45 +0700

    fix: improve spawner

--- Assets/Scripts/Services/SpawnerService.cs
+++ Assets/Scripts/Services/SpawnerService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
@@ -6,11 +7,7 @@ namespace Services
 {
     public class SpawnerService : Service
     {
-        [SerializeField] private Transform _playerStartPoint;
-        [SerializeField] private Transform _leftBorder;
-        [SerializeField] private Transform _rightBorder;
-        [SerializeField] private Transform _topBorder;
-        [SerializeField] private Transform _bottomBorder;
+        [SerializeField] private Transform _createionZone;
 
         [SerializeField] private int _coinsCount;
         [SerializeField] private int _bombsCount;
@@ -19,6 +16,9 @@ namespace Services
         [SerializeField] private GameObject _coinPrefab;
         [SerializeField] private GameObject _bombPrefab;
         [SerializeField] private GameObject _rocketPrefab;
+        
+        private const float BorderLimit = 0.5f;
+        private const float Distance = 1f;
 
         public override void Init()
         {
@@ -32,42 +32,42 @@ namespace Services
 
         private void CreateStaticObjects()
         {
-            var x = _playerStartPoint.position.x;
-            var leftLimit = _leftBorder.position.z;
-            var rightLimit = _rightBorder.position.z;
-            var topLimit = _topBorder.position.y;
-            var bottomLimit = _bottomBorder.position.y;
+            var coinsStep = Distance / _coinsCount;
+            var bombsStep = Distance / _bombsCount;
 
-            var distance = Mathf.Abs(leftLimit) + Mathf.Abs(rightLimit);
-            var coinsStep = distance / _coinsCount;
-            var bombsStep = distance / _bombsCount;
-
-            for (var z = leftLimit; z < rightLimit; z += coinsStep)
+            for (var z = -BorderLimit; z < BorderLimit; z += coinsStep)
             {
-                var y = Random.Range(bottomLimit, topLimit);
-                Instantiate(_coinPrefab, new Vector3(x, y, z), Quaternion.identity, transform);
+                var position = GetPosition(z);
+                Instantiate(_coinPrefab, position, Quaternion.identity, transform);
             }
 
-
-            for (var z = leftLimit; z < rightLimit; z += bombsStep)
+            for (var z = -BorderLimit; z < BorderLimit; z += bombsStep)
             {
-                var y = Random.Range(bottomLimit, topLimit);
-                Instantiate(_bombPrefab, new Vector3(x, y, z), Quaternion.identity, transform);
+                var position = GetPosition(z);
+                Instantiate(_bombPrefab, position, Quaternion.identity, transform);
             }
         }
 
         private IEnumerator CreateRockets()
         {
-            var x = _playerStartPoint.position.x;
-            var z = _leftBorder.position.z - 10;
-            var y = _topBorder.position.y;
-
+            var position = GetPosition(BorderLimit + 0.2f);
+            
             for (var i = 0; i < _maxRocketsCount; i++)
             {
-                Instantiate(_rocketPrefab, new Vector3(x, y, z), Quaternion.identity, transform);
-
+                Instantiate(_rocketPrefab, position, Quaternion.identity, transform);
                 yield return new WaitForSeconds(5f);
             }
+        }
+
+        private Vector3 GetPosition(float z)
+        {
+            var y = Random.Range(-BorderLimit, BorderLimit);
+            return _createionZone.TransformPoint(0, y, z);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireCube(_createionZone.position, _createionZone.localScale);
         }
     }
 }
